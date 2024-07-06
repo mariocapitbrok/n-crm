2592499eaafef2220c92b6887751c27dc3d92239
Luis Ramos <luis.ramos@salvosoftware.com>
2024-07-05 19:29:53 -0600

    Refactor Sidebar container styles

--- src/app/components/Sidebar/Container/index.tsx
+++ src/app/components/Sidebar/Container/index.tsx
@@ -8,15 +8,16 @@ interface Props {
 const Container = ({ isOpen, children }: Props) => {
   const md = `h-screen`;
 
-  return (
-    <div
-      className={`flex ${
-        isOpen ? 'absolute top-0 left-0 w-64' : `w-12 h-12 ${md}`
-      } bg-base-200 h-screen transition-width duration-300`}
-    >
-      <div className="flex flex-col w-full">{children}</div>
-    </div>
-  );
+  const styles = function () {
+    const name = 'SidebarContainer';
+    const open = `${name} absolute top-0 left-0 w-64`;
+    const closed = `${name} w-12 h-12 ${md}`;
+    const rest = `bg-base-200 h-screen transition-width duration-300`;
+
+    return `${isOpen ? open : closed} ${rest}`;
+  };
+
+  return <div className={styles()}>{children}</div>;
 };
 
 export default Container;
