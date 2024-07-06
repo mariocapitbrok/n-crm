a58dc92bc008b226d5ed9ba9c1adc6b71f57f884
Luis Ramos <luis.ramos@salvosoftware.com>
2024-07-05 21:05:09 -0600

    Implement UseMemo instead of functions

--- src/app/components/Sidebar/Container/Menu/MainLink/index.tsx
+++ src/app/components/Sidebar/Container/Menu/MainLink/index.tsx
@@ -1,5 +1,4 @@
-// components/MainLink.tsx
-import { FC } from 'react';
+import { FC, useMemo } from 'react';
 import Link from 'next/link';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
@@ -12,32 +11,22 @@ export interface MainLinkProps {
 }
 
 const MainLink: FC<MainLinkProps> = ({ href, icon, label, isOpen }) => {
-  const md = `md:flex md:w-4 md:h-5`;
+  const iconClass = useMemo(() => {
+    const base = 'mainlink-icon';
+    return isOpen ? `${base} w-4 h-5` : `${base} hidden md:flex md:w-4 md:h-5`;
+  }, [isOpen]);
 
-  const iconStyles = function () {
-    const name = 'mainlink-icon';
-    const open = `${name} w-4 h-5`;
-    const closed = `${name} hidden ${md}`;
-    const rest = ``;
-
-    return `${isOpen ? open : closed} ${rest}`;
-  };
-
-  const labelStyles = function () {
-    const name = 'mainlink-label';
-    const open = `${name} ml-4 `;
-    const closed = `${name} hidden`;
-    const rest = ``;
-
-    return `${isOpen ? open : closed} ${rest}`;
-  };
+  const labelClass = useMemo(() => {
+    const base = 'mainlink-label';
+    return isOpen ? `${base} ml-4` : `${base} hidden`;
+  }, [isOpen]);
 
   return (
     <li>
       <Link href={href} legacyBehavior>
         <a className="flex items-center">
-          <FontAwesomeIcon icon={icon} className={iconStyles()} />
-          <span className={labelStyles()}>{label}</span>
+          <FontAwesomeIcon icon={icon} className={iconClass} />
+          <span className={labelClass}>{label}</span>
         </a>
       </Link>
     </li>
